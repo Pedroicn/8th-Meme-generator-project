70e8e30398848cb68351368eb2c0df3defcae634
Pedro Nunes <pedro.icn88@gmail.com>
2022-06-05 10:24:10 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const boxText = document.querySelector('#meme-text');
 const inputImg = document.querySelector('#meme-insert');
 const boxImg = document.querySelector('#meme-image')
 const imgContainer = document.querySelector('#meme-image-container')
+const redButton = document.querySelector('#fire');
+const greenButton = document.querySelector('#earth');
+const blueButton = document.querySelector('#water');
+const buttonsSection = document.querySelector('#borderButtons');
+
 function textInsert() {
   boxText.innerText = textInput.value;
 }
@@ -15,3 +20,19 @@ function insertImg() {
 }
 
 inputImg.addEventListener('input', insertImg)
+
+function addBorderRed() {
+  imgContainer.style.border = '3px dashed rgb(255, 0, 0)'
+}
+
+function addBordeBlue() {
+  imgContainer.style.border = '5px double rgb(0, 0, 255)'
+}
+
+function addBorderGreen() {
+  imgContainer.style.border = '6px groove rgb(0, 128, 0)'
+}
+
+redButton.addEventListener('click', addBorderRed);
+blueButton.addEventListener('click', addBordeBlue);
+greenButton.addEventListener('click', addBorderGreen);
