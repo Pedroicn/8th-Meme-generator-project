1cca902ead7d406e55273ea7a8c7c8689bdc2353
Pedro Nunes <pedro.icn88@gmail.com>
2022-06-05 09:15:47 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const textInput = document.querySelector('#text-input');
 const boxText = document.querySelector('#meme-text');
-
+const inputImg = document.querySelector('#meme-insert');
+const boxImg = document.querySelector('#meme-image')
+const imgContainer = document.querySelector('#meme-image-container')
 function textInsert() {
   boxText.innerText = textInput.value;
 }
 
 textInput.addEventListener('input', textInsert)
+
+function insertImg() {
+  const image = URL.createObjectURL(inputImg.files[0]);
+  boxImg.src = image;
+}
+
+inputImg.addEventListener('input', insertImg)
